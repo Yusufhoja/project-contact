6c969dafe3aa6a17711bef2e7d61a21e719c5c77
Yusufhoja <usufhozah@gmail.com>
2024-07-27 12:47:51 +0500

    bitbalo

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ const sanoq1Element = document.getElementById('sanoq1');
 const intervalId = setInterval(() => {
     sanoq1Element.textContent = sanoq1;
     sanoq1++;
-    if (sanoq1 > 123) {
+    if (sanoq1 > 4  ) {
         clearInterval(intervalId);
     }
-}, 20); 
+}, 200); 
 let sanoq2 = 0;
 const sanoq2Element = document.getElementById('sanoq2');
 
 const intervalId2 = setInterval(() => {
     sanoq2Element.textContent = sanoq2;
     sanoq2++;
-    if (sanoq2 > 234) {
+    if (sanoq2 > 20) {
         clearInterval(intervalId2);
     }
 }, 10);
@@ -24,10 +24,10 @@ const sanoq3Element = document.getElementById('sanoq3');
 const intervalI3 = setInterval(() => {
     sanoq3Element.textContent = sanoq3;
     sanoq3++;
-    if (sanoq3 > 345) {
+    if (sanoq3 > 20000) {
         clearInterval(intervalI3);
     }
-}, 7); 
+}, 0.1); 
 
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
@@ -41,3 +41,26 @@ var swiper = new Swiper(".mySwiper", {
       clickable: true,
     },
   });
+
+
+  <script>
+    AOS.init();
+</script>
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const targetElement = document.querySelector('.box');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animate__animated', 'animate__bounceInLeft');
+                observer.unobserve(entry.target); // Faqat bir marta animatsiyani bajarish uchun
+            }
+        });
+    }, {
+        threshold: 0.5 // 50% ko'rinadigan bo'lganda animatsiya boshlanadi
+    });
+
+    observer.observe(targetElement);
+});
